9a7b0384859f02aa0870236960e87ed73126b066
amukh1 <fr0g_gam3r@outlook.com>
2025-09-28 21:54:52 -0400

    Set up vite w/ react, build, no deploy

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import './Home.css'
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div className="LIONSBYTE">LIONSBYTE</div>
+        </div>
+    )
+}
---
+++ src/Nav.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function Nav() {
+    return (
+        <div className="nav">
+            <div>navbar</div>
+        </div>
+    )
+}
